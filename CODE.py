af9017ec905587ee9d63d0de7b38804aa6506ca6
nafev <98347041+nafev@users.noreply.github.com>
2022-06-19 18:32:01 +1000

    Add files via upload

--- CODE.py
+++ CODE.py
@@ -13,39 +13,3 @@ if Vopros == 0:
             break
 
     n = p * q
-
-    while True:
-        M = int(input('Message: '))
-        if M>=n:
-            print("Enter a Message less")
-        else:
-            break
-
-
-
-
-    F = (p - 1) * (q - 1)
-    for i in range(2, F):
-        if (gcd(i, F) == 1):
-            e = i
-            break
-
-i = 0
-
-    while (i * e % F != 1):
-        i += 1
-        d = i
-        if i == e:
-            i += 1
-    C = M ** e % n
-    print("crypt message: ",C)
-    print(e)
-    print(d)
-    print(n)
-elif Vopros == 1:
-    print("Enter Private Key")
-    d = int(input("d = "))
-    n = int(input("n = "))
-    c = int(input("Enter what need to decrypt  = "))  # ie % F = 1 !@! id = 1 (mod F)
-    M = (c ** d) % n
-    print("Decrypt message = ",M)
