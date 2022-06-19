fb5d20598f841ec5c7fb1d4b2905a299207f61a9
nafev <98347041+nafev@users.noreply.github.com>
2022-06-19 18:23:35 +1000

    Add files via upload

--- CODE.py
+++ CODE.py
@@ -1,3 +1,19 @@
+from math import gcd
+import sympy
+
+Vopros = int(input("For Encryption Enter 0, For Decryption Enter 1 = "))
+if Vopros == 0:
+    print("Enter prime numbers")
+    while True:
+        p = int(input("p = "))
+        q = int(input("q = "))
+        if sympy.isprime(q) == False or sympy.isprime(p) == False:
+            print("Enter ONLY PRIME numbers")
+        else:
+            break
+
+    n = p * q
+
     while True:
         M = int(input('Message: '))
         if M>=n:
@@ -12,3 +28,21 @@
         if (gcd(i, F) == 1):
             e = i
             break
+    i = 0
+    while (i * e % F != 1):
+        i += 1
+        d = i
+        if i == e:
+            i += 1
+    C = M ** e % n
+    print("crypt message: ",C)
+    print(e)
+    print(d)
+    print(n)
+elif Vopros == 1:
+    print("Enter Private Key")
+    d = int(input("d = "))
+    n = int(input("n = "))
+    c = int(input("Enter what need to decrypt  = "))  # i*e % F = 1 !@! i*d = 1 (mod F)
+    M = (c ** d) % n
+    print("Decrypt message = ",M)
